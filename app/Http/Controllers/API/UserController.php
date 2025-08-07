360d60e5422a7f144309d5e0c603d70a5e4da758
AryaYubi <aryayubi@icloud.com>
2025-08-07 11:50:46 +0700

    test2

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Helpers\ResponseFormatter;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
@@ -19,7 +21,7 @@ class UserController extends Controller
                 'name' => ['required', 'string', 'max:255'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
                 'username' => ['required', 'string', 'max:255', 'unique:users'],
-                // 'phone' => ['nullable', 'string', 'max:12'],
+                'phone' => ['nullable', 'string', 'max:12'],
                 'password' => ['required', 'string', Password::min(8)],
                  ]);
 
@@ -27,7 +29,7 @@ class UserController extends Controller
                 'name' => $request->name,
                 'email' => $request->email,
                 'username' => $request->username,
-                // 'phone' => $request->phone,
+                'phone' => $request->phone,
                 'password' => Hash::make($request->password),
                 ]);
 
@@ -51,5 +53,66 @@ class UserController extends Controller
         );
 
         }
+    }
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => ['required', 'string', 'email'],
+                'password' => ['required', 'string'],
+            ]);
+            $credentials = $request->only('email', 'password');
+
+                if (!Auth::attempt($credentials)) {
+                    return ResponseFormatter::error(
+                        [
+                            'message' => 'Unauthorized',
+                        ],
+                        'Auth Failed', 500
+                    );
+
+                } else {
+                    $user = User::where('email', $request->email)->first();
+                    if(!Hash::check($request->password, $user->password,[])){
+                        throw new \Exception('Invalid credentials');
+                    }
+                    $tokenResult = $user->createToken('auth_token')->plainTextToken;
+
+                    return ResponseFormatter::success([
+                        'access_token' => $tokenResult,
+                        'token_type' => 'Bearer',
+                        'user' => $user,
+                    ], 'Auth Success');
+                }
+
+        } catch (Exception $error) {
+            return ResponseFormatter::error(
+                [
+                    'message' => 'Something went wrong',
+                    'error' => $error->getMessage(),
+                ],
+                'Authentication failed', 500
+            );
+        }
+    }
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success(
+            $request->user(),
+            'User data fetched successfully'
+        );
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success(
+            $user,
+            'User profile updated successfully'
+        );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,29 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+
+
+
+
+
+
+
 Route::get('products', [App\Http\Controllers\API\ProductController::class, 'all']);
 Route::get('categories', [App\Http\Controllers\API\ProductCategoryController::class, 'all']);
 
 Route::post('register', [App\Http\Controllers\API\UserController::class, 'register']);
+Route::post('login', [App\Http\Controllers\API\UserController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class,'fetch']) ;
+    Route::post('user', [UserController::class, 'updateProfile']);
+    });
